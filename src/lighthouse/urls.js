3987a3f4bfa9180f6b8af254fd8e1573f37094af
Andy Chosak <andy.chosak@cfpb.gov>
2020-09-08 12:08:01 -0400

    Update list of URLs
    See internal el-camino#292 for discussion.

--- src/lighthouse/urls.js
+++ src/lighthouse/urls.js
@@ -1,28 +1,28 @@
-// Most popular pages as of July 2, 2020, based on traffic from 4/1-6/30/2020
-// Source: hubcap/wiki/Current-Top-20-cf.gov-pages
 const urls = [
-  '/about-us/blog/guide-covid-19-economic-stimulus-checks/',
-  '/about-us/blog/guide-coronavirus-mortgage-relief-options/',
   '/',
-  '/complaint/',
-  '/find-a-housing-counselor/',
-  '/learnmore/',
-  '/ask-cfpb/what-is-the-best-way-to-negotiate-a-settlement-with-a-debt-collector-en-1447/',
-  '/coronavirus/',
-  '/consumer-tools/prepaid-cards/',
-  '/coronavirus/cares-act-mortgage-forbearance-what-you-need-know/',
+  '/about-us/blog/',
   '/about-us/blog/economic-impact-payment-prepaid-card/',
-  '/about-us/blog/what-you-need-to-know-about-student-loans-and-coronavirus-pandemic/',
-  '/complaint/getting-started/',
-  '/coronavirus/mortgage-and-housing-assistance/',
-  '/ask-cfpb/what-is-forbearance-en-289/',
-  '/ask-cfpb/what-should-i-do-when-a-debt-collector-contacts-me-en-1695/',
+  '/about-us/blog/guide-covid-19-economic-stimulus-checks/',
   '/about-us/blog/protect-yourself-financially-from-impact-of-coronavirus/',
   '/about-us/contact-us/',
-  '/coronavirus/managing-your-finances/economic-impact-payment-prepaid-debit-cards/',
+  '/ask-cfpb/',
   '/ask-cfpb/how-can-i-tell-who-owns-my-mortgage-en-214/',
+  '/ask-cfpb/what-is-a-debt-to-income-ratio-why-is-the-43-debt-to-income-ratio-important-en-1791/',
+  '/ask-cfpb/what-is-forbearance-en-289/',
+  '/ask-cfpb/what-is-the-best-way-to-negotiate-a-settlement-with-a-debt-collector-en-1447/',
+  '/ask-cfpb/what-should-i-do-when-a-debt-collector-contacts-me-en-1695/',
+  '/complaint/',
+  '/complaint/getting-started/',
+  '/consumer-tools/prepaid-cards/',
+  '/coronavirus/',
+  '/coronavirus/managing-your-finances/economic-impact-payment-prepaid-debit-cards/',
+  '/coronavirus/mortgage-and-housing-assistance/',
+  '/coronavirus/student-loans/',
+  '/find-a-housing-counselor/',
+  '/learnmore/',
+  '/owning-a-home/',
   '/policy-compliance/rulemaking/regulations/',
-  '/ask-cfpb/what-is-a-debt-to-income-ratio-why-is-the-43-debt-to-income-ratio-important-en-1791/'
+  '/start-small-save-up/start-saving/'
 ];
 
 module.exports = urls;
