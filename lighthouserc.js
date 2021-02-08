77859862d79de0f1d3bfa09878fb4f3babaf2230
Anselm Bradford <anselmbradford@users.noreply.github.com>
2021-02-08 12:38:45 -0500

    Fix config

--- lighthouserc.js
+++ lighthouserc.js
@@ -17,7 +17,8 @@ module.exports = {
     collect: {
       url: getUrls( baseUrl, mobile ),
       settings: {
-        formFactor: mobile ? 'mobile' : 'desktop'
+        formFactor: mobile ? 'mobile' : 'desktop',
+        screenEmulation: { mobile: mobile }
       }
     },
     upload: {
