eca7afe513db340b6b9b6ac1128b6efb00ca2ebd
Andy Chosak <andy.chosak@cfpb.gov>
2024-07-16 16:01:29 -0400

    Modernize linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,42 @@
-const fs = require( 'fs' );
-const nunjucks = require( 'nunjucks' );
-const nunjucksDateFilter = require( 'nunjucks-date-filter' );
-const path = require( 'path' );
-const filters = require( './scripts/lib/filters' );
+const fs = require("fs");
+const nunjucks = require("nunjucks");
+const nunjucksDateFilter = require("nunjucks-date-filter");
+const path = require("path");
+const filters = require("./scripts/lib/filters");
 
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
-const NunjucksWebpackPlugin = require( 'nunjucks-webpack-plugin' );
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const NunjucksWebpackPlugin = require("nunjucks-webpack-plugin");
 
 // eslint-disable-next-line no-process-env
-const RELATIVE_URL = process.env.LIGHTHOUSE_RELATIVE_URL || '';
+const RELATIVE_URL = process.env.LIGHTHOUSE_RELATIVE_URL || "";
 
 // eslint-disable-next-line no-sync
-const allReports = JSON.parse( fs.readFileSync( path.resolve( 'docs/reports.json' ) ) );
+const allReports = JSON.parse(
+  fs.readFileSync(path.resolve("docs/reports.json"))
+);
 
 const nunjucksEnvironment = nunjucks.configure();
-nunjucksEnvironment.addFilter( 'date', nunjucksDateFilter );
+nunjucksEnvironment.addFilter("date", nunjucksDateFilter);
 
-nunjucksEnvironment.addFilter( 'scoreIcon', filters.scoreIcon );
+nunjucksEnvironment.addFilter("scoreIcon", filters.scoreIcon);
 
 /**
  * Get a list of all the pages and their report data for Webpack to templatize.
- * @param {Object} reports Lighthouse reports organized by cf.gov page slug and date.
+ * @param {object} reports - Lighthouse reports organized by cf.gov page slug and date.
  * @returns {Array} List of templates to pass to nunjucks webpack plugin.
  */
-function getPageTemplates( reports ) {
+function getPageTemplates(reports) {
   const templates = [];
-  for ( const [ slug, summary ] of Object.entries( reports.pages ) ) {
-    templates.push( {
-      from: './src/templates/page.njk',
-      to: `${ slug }/index.html`,
+  for (const [slug, summary] of Object.entries(reports.pages)) {
+    templates.push({
+      from: "./src/templates/page.njk",
+      to: `${slug}/index.html`,
       context: {
         RELATIVE_URL,
-        url: summary[Object.keys( summary )[0]].url,
-        summaryReport: summary
-      }
-    } );
+        url: summary[Object.keys(summary)[0]].url,
+        summaryReport: summary,
+      },
+    });
   }
   return templates;
 }
@@ -43,63 +45,56 @@ function getPageTemplates( reports ) {
  * Create plugin to generate HTML files using Nunjucks templates. Plugin will
  * generate a Lighthouse summary HTML file for each subdirectory under
  * ./docs/reports, plus an index.html that links to each of those summary files.
- * @param {Object} reports All the Lighthouse reports read from reports.json.
+ * @param {object} reports - All the Lighthouse reports read from reports.json.
  * @returns {NunjucksWebpackPlugin} Webpack plugin to create HTML files.
  */
-function buildReportPlugin( reports ) {
-  return new NunjucksWebpackPlugin(
-    {
-      templates: [
-        {
-          from: './src/templates/index.njk',
-          to: 'index.html',
-          context: {
-            RELATIVE_URL,
-            pages: allReports.pages
-          }
+function buildReportPlugin(reports) {
+  return new NunjucksWebpackPlugin({
+    templates: [
+      {
+        from: "./src/templates/index.njk",
+        to: "index.html",
+        context: {
+          RELATIVE_URL,
+          pages: allReports.pages,
         },
-        ...getPageTemplates( reports )
-      ],
-      configure: nunjucksEnvironment
-    }
-  );
+      },
+      ...getPageTemplates(reports),
+    ],
+    configure: nunjucksEnvironment,
+  });
 }
 
-module.exports = ( env, argv ) => {
-
+module.exports = (env, argv) => {
   const config = {
-    mode: argv.mode || 'development',
+    mode: argv.mode || "development",
     entry: {
-      main: [ './src/css/main.scss', './src/js/main.js' ]
+      main: ["./src/css/main.scss", "./src/js/main.js"],
     },
     output: {
-      path: path.resolve( __dirname, 'docs' ),
-      filename: 'static/js/[name].js'
+      path: path.resolve(__dirname, "docs"),
+      filename: "static/js/[name].js",
     },
     plugins: [
-      new MiniCssExtractPlugin(
-        {
-          filename: 'static/css/[name].css'
-        }
-      ),
-      buildReportPlugin( allReports )
+      new MiniCssExtractPlugin({
+        filename: "static/css/[name].css",
+      }),
+      buildReportPlugin(allReports),
     ],
     module: {
       rules: [
         {
           test: /\.scss$/,
           exclude: /node_modules/,
-          use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
         },
         {
           test: /\.js$/,
-          exclude: [
-            path.resolve( __dirname, 'node_modules' )
-          ],
-          use: [ 'babel-loader' ]
-        }
-      ]
-    }
+          exclude: [path.resolve(__dirname, "node_modules")],
+          use: ["babel-loader"],
+        },
+      ],
+    },
   };
 
   return config;
